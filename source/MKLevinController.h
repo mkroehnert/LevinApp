320e42e28c43a236378ce25815192de6ea9b1215
mkroehnert <mkroehnert@users.sourceforge.net>
2009-11-15 17:39:23 +0100

    [MKLevinController] added new controller class

---
+++ source/MKLevinController.h
@@ -0,0 +1,16 @@
+//
+//  MKLevinController.h
+//  Levin
+//
+//  Created by MK on 15.11.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MKLevinController : NSObject {
+
+}
+
+@end
---
+++ source/MKLevinController.m
@@ -0,0 +1,14 @@
+//
+//  MKLevinController.m
+//  Levin
+//
+//  Created by MK on 15.11.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "MKLevinController.h"
+
+
+@implementation MKLevinController
+
+@end
