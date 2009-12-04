58434a013f8ee1bdd8fbb99f7993f9ce94384bba
mkroehnert <mkroehnert@users.sourceforge.net>
2009-12-05 00:15:04 +0100

    [License] add License.txt containing GPL v3 and add GPL Headers in each source file

--- source/MKPathTransformer.m
+++ source/MKPathTransformer.m
@@ -3,8 +3,24 @@
 //  Levin
 //
 //  Created by MK on 20.11.09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+//  Copyright 2009 Manfred Kroehnert
+
+/*
+ This file is part of Levin.
+ 
+ Levin is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Levin is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Levin.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "MKPathTransformer.h"
 
